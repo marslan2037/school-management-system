ce5e3a1126f212093748314cb922b47025188b3a
Muhammad Arslan <m.arslan2037@gmail.com>
2022-04-03 01:36:46 +0500

    dashboard component is added

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/authentication/login/login.component';
 import { SignupComponent } from './components/authentication/signup/signup.component';
 import { ForgetPasswordComponent } from './components/authentication/forget-password/forget-password.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    ForgetPasswordComponent
+    ForgetPasswordComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
