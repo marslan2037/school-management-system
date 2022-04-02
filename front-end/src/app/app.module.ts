62cae6b87186f77752b2a77b7608a68514a55e91
Muhammad Arslan <m.arslan2037@gmail.com>
2022-04-03 01:33:27 +0500

    login, signup, and forget-password components are added

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { SignupComponent } from './components/authentication/signup/signup.component';
+import { ForgetPasswordComponent } from './components/authentication/forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetPasswordComponent
   ],
   imports: [
     BrowserModule,
